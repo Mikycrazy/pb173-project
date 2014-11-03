b091aeef8af9867f77fedf5bc4cfc19edbc73bf3
Michal Valenta <michvalenta@gmail.com>
2014-11-03 22:29:12 +0100

    Comments and makefiles

--- ClientApplication/ClientApplication/Client.h
+++ ClientApplication/ClientApplication/Client.h
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string.h>
-#include "Crypto.h"
-#include "Network.h"
+#include "CryptoManager.h"
+#include "NetworkManager.h"
 
 class Client
 {
@@ -24,6 +24,13 @@ private:
 	int mReceiverPort;
 
 public:
+	/**
+	* Konstruktor pro tridu Client.
+	* Nastavi username a email novemu clientovi.
+	*
+	* @param username		uzivatelke jmeno
+	* @param email			email
+	*/
 	Client(std::string username, std::string email);
 	~Client();
 
@@ -33,21 +40,54 @@ public:
 	std::string Email() const	{ return mEmail; }
 	void Email(std::string email) { mEmail = email; }
 
-
+	/**
+	* Vygenerovani novych rsa klicu.
+	*/
 	int generateNewRSAKeys();
+	/**
+	* Vygenerovani noveho AES klice.
+	*/
 	int generateNewAESKey();
-
+	/**
+	* Odesle pozadavek na prihlaseni na server.
+	*/
 	int login();
+	/**
+	* Odesle pozadavek na odhlaseni na server.
+	*/
 	int logout();
 
+	/**
+	* Odesle pozadavek na vydani certifikatu uzivatele, na certifikacni autoritu
+	*
+	* @param username		uzivatelske jmeno 
+	*/
 	int getCertificat(std::string username);
+	/**
+	* Odesle pozadavek k registraci s username a email na certifikacni autoritu.
+	*/
 	int registerToCA();
 
+	/**
+	* Odesle pozadavek ke komunikaci ke klientovi
+	*
+	* @param username		uzivatelske jmeno 
+	*/
 	int connectToClient(std::string username);
+
+	/**
+	* Zahaji komunikaci s klientem
+	*
+	* @param data		data
+	*/
 	int startCommunication(unsigned char* data);
 
 private:
+	/**
+	* Zpracuje prisla data
+	*
+	* @param data		data
+	*/
 	int processData(unsigned char* data);
-	int sendData(std::string IP, std::string port, unsigned char* data);
 };
 
