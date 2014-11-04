90e9d4ff20f108efaf4b513ecf80bec1e5a9a64c
PanSpagetka <xknaur@fi.muni.cz>
2014-11-04 16:45:07 +0100

    Add test files
    pridal sem ty testovy fily co sme delali na cviceni

---
+++ ClientApplication/ClientApplication/ClientApplicationTests.cpp
@@ -0,0 +1,60 @@
+
+TEST_CASE("SERVER")
+{
+
+	SECTION("is avaible")
+	{
+
+	}
+	SECTION("is not avaible")
+	{
+
+	}
+
+}
+TEST_CASE("PACKET CREATING")
+{
+	SECTION("login packet")
+	{
+
+	}
+	SECTION("logout packet")
+	{
+
+	}
+	SECTION("get online list packet")
+	{
+
+	}
+}
+
+TEST_CASE("SENDING DATA")
+{
+	SECTION("send data match")
+	{
+
+	}
+	SECTION("send data dont match")
+	{
+
+	}
+}
+TEST_CASE("RECIEVING DATA")
+{
+	SECTION("login succesful")
+	{
+
+	}
+	SECTION("login not succesful")
+	{
+
+	}
+	SECTION("request list of users when not logged in")
+	{
+
+	}
+	SECTION("request list of users when logged in")
+	{
+
+	}
+}
---
+++ Server/Server/ServerTest.cpp
@@ -0,0 +1,88 @@
+
+TEST_CASE("LOGGING")
+{
+
+	SECTION("log in")
+	{
+
+	}
+	SECTION("log out")
+	{
+
+	}
+	SECTION("get online list")
+	{
+
+	}
+
+}
+TEST_CASE("PACKET CREATING")
+{
+	SECTION("login response packet")
+	{
+
+	}
+	SECTION("logout response packet")
+	{
+
+	}
+	SECTION("get online list response packet")
+	{
+
+	}
+}
+
+TEST_CASE("SENDING DATA")
+{
+	SECTION("send data match")
+	{
+
+	}
+	SECTION("send data dont match")
+	{
+
+	}
+}
+TEST_CASE("RECIEVING DATA")
+{
+	SECTION("login request succesful - change mUsers")
+	{
+
+	}
+	SECTION("login request not succesful - mUsers not changed")
+	{
+
+	}
+	SECTION("request list of users when not logged in - request denied")
+	{
+
+	}
+	SECTION("request list of users when logged in - response send")
+	{
+
+	}
+}
+TEST_CASE("LOGIN/LOGOUT USERS")
+{
+	int userCount;
+	SECTION("add first user")
+	{
+		
+	}
+	SECTION("add more users")
+	{
+		
+	}
+	SECTION("remove user")
+	{
+
+	}
+	SECTION("remove all users")
+	{
+
+	}
+	SECTION("remove from empy users list")
+	{
+
+	}
+}
